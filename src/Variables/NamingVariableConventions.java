ed1695802a3be2f228246b31d23606b7e32a203d
Darron Brown <DarronBrown88@outlook.com>
2018-01-15 13:41:46 -0700

    Naming Variable Conventions

---
+++ src/Variables/NamingVariableConventions.java
@@ -0,0 +1,15 @@
+package Variables;
+
+class NamingVariableConventions {
+	public static void main(String[] args) {
+		/*
+			variable names should respresent the information
+		*/
+
+		String firstName = "Darron";
+		String middleName = "Reginald";
+		String lastName = "Brown";
+
+		System.out.println(firstName + " " + middleName + " " + lastName);
+	}
+}
