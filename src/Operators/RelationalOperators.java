9b36b54a104d391c9e4c786f4cdb7b3ac3e3d7f6
Darron Brown <DarronBrown88@outlook.com>
2018-01-21 16:23:38 -0700

    Relational Operators

---
+++ src/Operators/RelationalOperators.java
@@ -0,0 +1,30 @@
+package Operators;
+
+public class RelationalOperators {
+	public static void main(String[] args) {
+		
+		/*
+            Relational operators (comparison operators)
+            
+            == - it compares two variables and checks if variables are equal
+            != - checks if two variables are not equal to each other
+            > - greater than
+            >= - greater than or equal to
+            < - less than
+            <= - less than or equal
+        */
+        
+        int a = 5,
+            b = 5;
+        
+        boolean isTrue = false;
+        System.out.println(isTrue);
+        
+        if (a > b) {
+            System.out.println("test");
+        } else {
+        	System.out.println("numbers are equal");
+        }
+
+	}
+}
