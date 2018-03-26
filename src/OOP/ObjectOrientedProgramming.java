e8d84eeb4fb26f212fce42e0c07f7b33f3dbfd30
Darron Brown <DarronBrown88@outlook.com>
2018-03-25 19:42:44 -0600

    introduction to object oriented programming

---
+++ src/OOP/ObjectOrientedProgramming.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OOP;
+
+/**
+ *
+ * @author DarronBrown
+ */
+public class ObjectOrientedProgramming {
+    public static void main(String[] args) {
+        /*
+            Objects - containers that stores variables and functions thematically 
+                      connected to each other for easier future usage
+            Classes - blueprint for creating instances (copies) of objects      
+            
+            Properties - features (part of something)
+            Methods - functions 
+        */
+        
+        Monitor monitor1 = new Monitor();
+        
+        monitor1.width = 1000;
+        
+        monitor1.getWidth();
+        
+    }
+}
+
+class Monitor {
+    // Properties
+    int width;
+    int height;
+    int brightness;
+    
+    // Methods
+    void changeBrightness() {
+        /*
+            THIS IS PLACE FOR INSTRUCTION TO BE DONE
+        */
+    }
+    
+    void getWidth() {
+        System.out.println(width);
+    }
+}
+
+
